43e86c569a4297c3d74d6c058f470289f5f22e44
Sm1rfa <sm1rfs88@gmail.com>
2020-10-18 15:05:38 +0300

    +update: refactoring of logic

--- Employees/Employees.Desktop/Helpers/EmployeeDataHelper.cs
+++ Employees/Employees.Desktop/Helpers/EmployeeDataHelper.cs
@@ -3,11 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
-using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Windows;
 
 namespace Employees.Desktop.Helpers
 {
@@ -33,9 +30,9 @@ namespace Employees.Desktop.Helpers
                         {
                             Id = int.Parse(data[0].Trim()),
                             ProjectId = int.Parse(data[1].Trim()),
-                            DateFrom = DateParse(data[2].Trim()),
-                            DateTo = DateParse(data[3].Trim()),
-                            TotalDays = (DateParse(data[3].Trim()) - DateParse(data[2].Trim())).Days
+                            DateFrom = Utils.DateParse(data[2].Trim()),
+                            DateTo = Utils.DateParse(data[3].Trim()),
+                            TotalDays = (Utils.DateParse(data[3].Trim()) - Utils.DateParse(data[2].Trim())).Days
                         });
                     }
 
@@ -63,16 +60,8 @@ namespace Employees.Desktop.Helpers
             // to not return null and result in error in the visual three, could be handled better
             ObservableCollection<Employee> employeeCollection = new ObservableCollection<Employee>();
 
-            // create pairs array with all possible combinations - need to improve logic
-            int[] workerIds = employeeList.Select(e => e.Id).Distinct().ToArray();
-            List<KeyValuePair<int, int>> pairList = new List<KeyValuePair<int, int>>();
-            for (int i = 0; i < workerIds.Length; i++)
-            {
-                for (int j = 0; j < workerIds.Length; j++)
-                {
-                    pairList.Add(new KeyValuePair<int, int>(workerIds[i], workerIds[j]));
-                }
-            }
+            // create pairs array with all possible combinations
+            List<KeyValuePair<int, int>> pairList = GetEmployeeIds(employeeList);
 
             // distinct the values based on equallity
             var distinctPairs = pairList.Where(x => x.Key != x.Value).ToList();
@@ -92,42 +81,7 @@ namespace Employees.Desktop.Helpers
                 }
 
                 // we have nested iterration to map the data correctly
-                foreach (var pair in distinctPairs)
-                {
-                    foreach (var item in projectList)
-                    {
-                        if (item.Employees.Any(x => x.Id == pair.Key) && item.Employees.Any(x => x.Id == pair.Value))
-                        {
-                            EmployeeBase empl1 = employeeList.Where(x => x.Id == pair.Key && x.ProjectId == item.ProjectId).FirstOrDefault();
-                            EmployeeBase empl2 = employeeList.Where(x => x.Id == pair.Value && x.ProjectId == item.ProjectId).FirstOrDefault();
-                            // check if dates overlap to see if they actually met each other on the project
-                            if (CheckIfDatesOverlap(empl1.DateFrom, empl1.DateTo, empl2.DateFrom, empl2.DateTo))
-                            {
-                                int totalDays = CalculateDaysTogether(empl1.DateFrom, empl1.DateTo, empl2.DateFrom, empl2.DateTo);
-                                // we make a check if the object exists, we add project to the counter, else we create a new object
-                                if (employees.Where(y => y.EmployeeId == pair.Key && y.Employee2Id == pair.Value).Count() >= 1)
-                                {
-                                    EmployeesProjectCounter employee = employees.Where(x => x.EmployeeId.Equals(pair.Key) && x.Employee2Id.Equals(pair.Value)).FirstOrDefault();
-                                    employee.ProjectsTogether += 1; // could be removed
-                                    employee.ProjectDays.Add(new ProjectDays { ProjectId = item.ProjectId, TotalDays = totalDays });
-                                }
-                                else
-                                {
-                                    employees.Add(new EmployeesProjectCounter
-                                    {
-                                        EmployeeId = pair.Key,
-                                        Employee2Id = pair.Value,
-                                        ProjectsTogether = 1, // could be removed
-                                        ProjectDays = new List<ProjectDays> { new ProjectDays { ProjectId = item.ProjectId, TotalDays = totalDays } }
-                                    });
-                                }
-                            }
-                        }
-                    }
-                }
-
-                List<EmployeesProjectCounter> orderLongestTime = employees.OrderByDescending(x => x.ProjectDays.Max(y => y.TotalDays)).ToList();
-                EmployeesProjectCounter longestTimeOnProjectByColleagues = orderLongestTime[0];
+                EmployeesProjectCounter longestTimeOnProjectByColleagues = MapPairsProjectsAndTotalDays(employeeList, distinctPairs, employees, projectList);
 
                 // getting the information about the two employees and their requisites [no necessity of lists, reusing old logic]
                 List<EmployeeBase> employee1 = employeeList.Where(e => e.Id.Equals(longestTimeOnProjectByColleagues.EmployeeId) && e.ProjectId.Equals(longestTimeOnProjectByColleagues.ProjectDays.Max(y => y.ProjectId))).ToList();
@@ -158,6 +112,75 @@ namespace Employees.Desktop.Helpers
             }
 
             return employeeCollection;
+        }
+
+        /// <summary>
+        /// Method to handle the logic for pair workers, their projects and calculate the total days
+        /// </summary>
+        /// <param name="employeeList"></param>
+        /// <param name="distinctPairs"></param>
+        /// <param name="employees"></param>
+        /// <param name="projectList"></param>
+        /// <returns></returns>
+        private static EmployeesProjectCounter MapPairsProjectsAndTotalDays(List<EmployeeBase> employeeList, List<KeyValuePair<int, int>> distinctPairs, List<EmployeesProjectCounter> employees, List<ProjectInfo> projectList)
+        {
+            foreach (var pair in distinctPairs)
+            {
+                foreach (var item in projectList)
+                {
+                    if (item.Employees.Any(x => x.Id == pair.Key) && item.Employees.Any(x => x.Id == pair.Value))
+                    {
+                        EmployeeBase empl1 = employeeList.Where(x => x.Id == pair.Key && x.ProjectId == item.ProjectId).FirstOrDefault();
+                        EmployeeBase empl2 = employeeList.Where(x => x.Id == pair.Value && x.ProjectId == item.ProjectId).FirstOrDefault();
+                        // check if dates overlap to see if they actually met each other on the project
+                        if (Utils.CheckIfDatesOverlap(empl1.DateFrom, empl1.DateTo, empl2.DateFrom, empl2.DateTo))
+                        {
+                            int totalDays = Utils.CalculateDaysTogether(empl1.DateFrom, empl1.DateTo, empl2.DateFrom, empl2.DateTo);
+                            // we make a check if the object exists, we add project to the counter, else we create a new object
+                            if (employees.Where(y => y.EmployeeId == pair.Key && y.Employee2Id == pair.Value).Count() >= 1)
+                            {
+                                EmployeesProjectCounter employee = employees.Where(x => x.EmployeeId.Equals(pair.Key) && x.Employee2Id.Equals(pair.Value)).FirstOrDefault();
+                                employee.ProjectsTogether += 1; // could be removed
+                                employee.ProjectDays.Add(new ProjectDays { ProjectId = item.ProjectId, TotalDays = totalDays });
+                            }
+                            else
+                            {
+                                employees.Add(new EmployeesProjectCounter
+                                {
+                                    EmployeeId = pair.Key,
+                                    Employee2Id = pair.Value,
+                                    ProjectsTogether = 1, // could be removed
+                                    ProjectDays = new List<ProjectDays> { new ProjectDays { ProjectId = item.ProjectId, TotalDays = totalDays } }
+                                });
+                            }
+                        }
+                    }
+                }
+            }
+
+            List<EmployeesProjectCounter> orderLongestTime = employees.OrderByDescending(x => x.ProjectDays.Max(y => y.TotalDays)).ToList();
+            EmployeesProjectCounter longestTimeOnProjectByColleagues = orderLongestTime[0];
+            return longestTimeOnProjectByColleagues;
+        }
+
+        /// <summary>
+        /// Method to create pairs [logic can be improved]
+        /// </summary>
+        /// <param name="employeeList"></param>
+        /// <returns></returns>
+        private static List<KeyValuePair<int, int>> GetEmployeeIds(List<EmployeeBase> employeeList)
+        {
+            int[] workerIds = employeeList.Select(e => e.Id).Distinct().ToArray();
+            List<KeyValuePair<int, int>> pairList = new List<KeyValuePair<int, int>>();
+            for (int i = 0; i < workerIds.Length; i++)
+            {
+                for (int j = 0; j < workerIds.Length; j++)
+                {
+                    pairList.Add(new KeyValuePair<int, int>(workerIds[i], workerIds[j]));
+                }
+            }
+
+            return pairList;
         }
 
         /// <summary>
@@ -195,55 +218,6 @@ namespace Employees.Desktop.Helpers
             }
 
             return employeeCollection;
-        }
-
-        /* from here, move to <utils> */
-
-        private static DateTime DateParse(string date)
-        {
-            DateTime dateValue;
-            // to handle the "today" state
-            if (date.Equals("NULL"))
-            {
-                return DateTime.Today;
-            }
-            // to handle eventual user input case, with different culture
-            if (DateTime.TryParse(date, CultureInfo.CurrentCulture, DateTimeStyles.None, out dateValue))
-            {
-                return dateValue;
-            }
-
-            // to handle if formatting and parsing are done from various cultures
-            dateValue = DateTime.Parse(date, CultureInfo.InvariantCulture);
-
-            return dateValue;
-        }
-
-        private static bool CheckIfDatesOverlap(DateTime startA, DateTime endA, DateTime startB, DateTime endB)
-        {
-            return startA < endB && startB < endA;
-        }
-
-        private static int CalculateDaysTogether(DateTime startA, DateTime endA, DateTime startB, DateTime endB) 
-        {
-            if (startA < startB && endA < endB) 
-            {
-                return Math.Abs((startB - endA).Days);
-            }
-            if (startA < startB && endA > endB)
-            {
-                return Math.Abs((startB - endB).Days);
-            }
-            if (startA > startB && endA < endB)
-            {
-                return Math.Abs((startA - endA).Days);
-            }
-            if (startA > startB && endA > endB)
-            {
-                return Math.Abs((startB - endB).Days);
-            }
-
-            return 1;
         }
     }
 }
---
+++ Employees/Employees.Desktop/Helpers/Utils.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Globalization;
+
+namespace Employees.Desktop.Helpers
+{
+    public static class Utils
+    {
+        public static DateTime DateParse(string date)
+        {
+            DateTime dateValue;
+            // to handle the "today" state
+            if (date.Equals("NULL"))
+            {
+                return DateTime.Today;
+            }
+            // to handle eventual user input case, with different culture
+            if (DateTime.TryParse(date, CultureInfo.CurrentCulture, DateTimeStyles.None, out dateValue))
+            {
+                return dateValue;
+            }
+
+            // to handle if formatting and parsing are done from various cultures
+            dateValue = DateTime.Parse(date, CultureInfo.InvariantCulture);
+
+            return dateValue;
+        }
+
+        public static bool CheckIfDatesOverlap(DateTime startA, DateTime endA, DateTime startB, DateTime endB)
+        {
+            return startA < endB && startB < endA;
+        }
+
+        public static int CalculateDaysTogether(DateTime startA, DateTime endA, DateTime startB, DateTime endB)
+        {
+            if (startA < startB && endA < endB)
+            {
+                return Math.Abs((startB - endA).Days);
+            }
+            if (startA < startB && endA > endB)
+            {
+                return Math.Abs((startB - endB).Days);
+            }
+            if (startA > startB && endA < endB)
+            {
+                return Math.Abs((startA - endA).Days);
+            }
+            if (startA > startB && endA > endB)
+            {
+                return Math.Abs((startB - endB).Days);
+            }
+
+            return 1;
+        }
+    }
+}
